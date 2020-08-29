2ab4c79c2b2d49a3793eafc757af6f61abbb5e51
JuoJuo <348107781@qq.com>
2020-08-29 21:19:31 +0800

    A模块使用B模块的功能，首先要在A模块importB模块，B的controller service才能使用！

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [], // 依赖外面的模块(可以是自己创建的比如userModule，或者是官方提供的比如typeorm, graphql,或者第三方的)
+  imports: [UserModule], // 依赖外面的模块(可以是自己创建的比如userModule，或者是官方提供的比如typeorm, graphql,或者第三方的)
   controllers: [AppController], // 该模块所用到的控制器
   providers: [AppService], // 该模块的提供者
   exports: [], // 别的模块要使用该模块中的某几个方法，就要在这里对外暴漏
